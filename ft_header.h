c4a3526b1b24876f88fcf2c691b7a89565d7a590
Nirina Prosper Herimaminavalona <nherimam@c1r1s5.42antananarivo.mg>
2024-03-08 12:00:10 +0300

    all_first

---
+++ ft_header.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_header.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nherimam <nherimam@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/08 11:47:22 by nherimam          #+#    #+#             */
+/*   Updated: 2024/03/08 11:47:25 by nherimam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_HEADER_H
+# define FT_HEADER_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+void	ft_putchar(char c);
+
+#endif
---
+++ ft_utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nherimam <nherimam@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/08 11:48:49 by nherimam          #+#    #+#             */
+/*   Updated: 2024/03/08 11:48:52 by nherimam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_header.h"
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+
+int	main(void)
+{
+	char	c = 'b';
+
+	ft_putchar (c);
+	ft_putstr ("salut");
+	return (0);
+}
